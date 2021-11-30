719db843918f19b0ae3274da062f728d575ac56f
Léo Azevedo <leofrancoazevedo@gmail.com>
2021-11-30 05:30:22 -0300

    Add project files.

---
+++ Estudo Csharp/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudo_Csharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
