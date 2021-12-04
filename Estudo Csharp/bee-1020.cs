2ca10f35883f97a4c694affad421c8b177306527
Léo Azevedo <leofrancoazevedo@gmail.com>
2021-12-04 17:29:06 -0300

    04/12 Parte2

---
+++ Estudo Csharp/bee-1020.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudo_Csharp
+{
+    class bee_1020
+    {
+        public static void X
+            (String [] args)
+        {
+            int Bruto, Ano, Mes, Dia;
+
+            Bruto = int.Parse(Console.ReadLine());
+
+            Ano = Bruto / 365;
+            Mes = (Bruto % 365) / 30;
+            Dia = ((Bruto % 365) % 30);
+
+            Console.WriteLine(Ano + " ano(s)");
+            Console.WriteLine(Mes + " mes(es)");
+            Console.WriteLine(Dia + " dia(s)");
+        }
+    }
+}
