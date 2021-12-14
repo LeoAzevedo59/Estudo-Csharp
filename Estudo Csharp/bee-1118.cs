5e9bb152fe77f8d4f62792c7912db214a37c1614
Léo Azevedo <leofrancoazevedo@gmail.com>
2021-12-13 21:41:31 -0300

    Exercicio 13/12

---
+++ Estudo Csharp/bee-1118.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudo_Csharp
+{
+    class bee_1118
+    {
+        public static void x(String[] vet)
+        {
+            double Media, X, Y;
+            int Opcao;
+
+            X = 0;
+            Y = 0;
+            Opcao = 0;
+
+            X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (X >= 0 || X <= 10)
+            {
+                while (Opcao != 2)
+                {
+                    while (X < 0 || X > 10)
+                    {
+                        Console.WriteLine("nota invalida");
+                        X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    }
+
+                    Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    while (Y < 0 || Y > 10)
+                    {
+                        Console.WriteLine("nota invalida");
+                        Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    }
+
+                    Media = (X + Y) / 2.0;
+                    Console.WriteLine("media = " + Media.ToString("F2", CultureInfo.InvariantCulture));
+
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    Opcao = int.Parse(Console.ReadLine());
+
+                    while (Opcao != 2 && Opcao != 1)
+                    {
+                        Console.WriteLine("novo calculo (1-sim 2-nao)");
+                        Opcao = int.Parse(Console.ReadLine());
+                    }
+
+                    if (Opcao == 1)
+                        X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+            }
+        }
+    }
+}
---
+++ Estudo Csharp/bee-1131.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudo_Csharp
+{
+    class bee_1131
+    {
+        public static void Main(String[] args)
+        {
+            int Inter, Gremio, Grenais, GolInter, GolGremio, Empate, Total;
+
+            Grenais = 1;
+            Empate = 0;
+            GolInter = 0;
+            GolGremio = 0;
+
+            while (Grenais == 1)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                Inter = int.Parse(vet[0]);
+                Gremio = int.Parse(vet[1]);
+
+                if (Inter > Gremio)
+                    GolInter += 1;
+                else if (Gremio > Inter)
+                    GolGremio += 1;
+                else
+                    Empate += 1;
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                Grenais = int.Parse(Console.ReadLine());
+            }
+
+            Total = GolGremio + GolInter + Empate;
+
+            Console.WriteLine(Total + " grenais");
+            Console.WriteLine("Inter: " + GolInter);
+            Console.WriteLine("Gremio: " + GolGremio);
+            Console.WriteLine("Empate: " + Empate);
+
+            if (GolInter > GolGremio)
+                Console.WriteLine("Inter venceu mais");
+            else if (GolGremio > GolInter)
+                Console.WriteLine("Gremio venceu mais");
+            else
+                Console.WriteLine("Nao houve vencedor");
+        }
+    }
+}
