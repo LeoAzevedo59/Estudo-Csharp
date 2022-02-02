57857c40db0d3c8d5bb6a9a4062b8ec53c27db03
Léo Azevedo <leofrancoazevedo@gmail.com>
2022-02-02 00:51:29 -0300

    estudos 02/02

---
+++ CommentPost/Entities/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentPost.Entities
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+    
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text) : this ()
+        {
+            Text = text;
+        }
+    }
+
+}
---
+++ CommentPost/Program.cs
@@ -0,0 +1,24 @@
+﻿using CommentPost.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CommentPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zeland",
+                "I'm going to visit this wonderful country!",
+                21
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+        }
+    }
+}
